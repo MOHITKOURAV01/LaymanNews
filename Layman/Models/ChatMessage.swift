79c081c04b20c0776fe1277eb53157a6e39add86
MOHITKOURAV01 <mohitdas852@gmail.com>
2026-04-23 23:15:22 +0530

    Models: add ChatMessage model

---
+++ Layman/Models/ChatMessage.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+struct ChatMessage: Identifiable, Sendable {
+    let id: UUID
+    let content: String
+    let isUser: Bool
+    let timestamp: Date
+
+    init(content: String, isUser: Bool) {
+        self.id = UUID()
+        self.content = content
+        self.isUser = isUser
+        self.timestamp = Date()
+    }
+}
